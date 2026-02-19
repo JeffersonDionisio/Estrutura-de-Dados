68ef1814fd753e1d778ea39b5ba4a8cc75bf02d4
JeffersonDionisio <jefferson.dionisio@outlook.com>
2026-02-19 20:10:40 -0300

    Create Arrays.java

---
+++ PROCESUAL/Arrays.java
@@ -0,0 +1,13 @@
+ARRAYS UNIDIMENSIONAIS EM JAVA
+
+Os arrays unidemensionais sã os que possuem apemas um indice pra acessa seu conteudo
+  eles são declarads da seguinte forma:
+Tipo de dado[] Nome do array = New tipo-de-dado [QUANTIDADE]
+
+Tipo de dado - Pode ser qualuer tipo de variável
+Nome do array - um nome válido; as mesmas regras para nome de variáveis
+Quantidade - Quantidade de elementos que o array pode manipular.
+
+  Exemplos:
+
+  INT[] Numeros = New INT[10]. Cria um array com o numero que contém 10 elementos do tipo int e seu indice varia de 0 a 9.
